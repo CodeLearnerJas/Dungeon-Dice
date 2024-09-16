aa378976b6f537d501923196623fc08c1b177412
CodeLearnerJas <yangtianjin2013@gmail.com>
2024-09-16 11:49:32 +1000

    Initial Commit

---
+++ Dungeon Dice/Dungeon_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_DiceApp.swift
+//  Dungeon Dice
+//
+//  Created by GuitarLearnerJas on 16/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
